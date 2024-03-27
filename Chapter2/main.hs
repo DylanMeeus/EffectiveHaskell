01434e7beb096555fa8e9e92e32d1ab742965425
Dylan Meeus <meeusdylan@protonmail.com>
2024-03-26 21:31:45 -0400

    list comprehension

--- Chapter2/main.hs
+++ Chapter2/main.hs
@@ -32,3 +32,44 @@ isBalanced s =
 myMap f l
     | [] == l = []
     | otherwise = f (head l) : myMap f (tail l)
+
+
+
+checkGuestList guestList name = elem name guestList
+
+foodCosts = [("Ren", 10.00), ("George", 4.00), ("Porter", 27.50)]
+
+partyBudget isAttending = foldr1 (+) . map snd . filter (isAttending . fst)
+
+
+-- map the list to the foodCosts
+-- sum for the second value
+
+partyBudget' attendees =  foldr1 (+) $ map snd $ filter (\x -> elem (fst x) attendees) foodCosts
+
+
+pairs as bs = 
+    let as' = filter (`elem` bs) as
+        bs' = filter odd bs
+        mkPairs a = map (\b -> (a,b)) bs'
+    in map mkPairs as'
+
+
+combineLists as bs 
+    | [] == as = []
+    | [] == bs = []
+    | otherwise = (head as, head bs) : combineLists (tail as) (tail bs)
+
+
+pairwiseSum xs ys =
+    let sumElems pairs = 
+            let a = fst pairs
+                b = snd pairs
+            in a + b
+    in map sumElems $ zip xs ys
+
+
+pairwiseSum' xs ys 
+    | [] == xs = []
+    | [] == ys = []
+    | otherwise = (head xs + head ys) : pairwiseSum' (tail xs) (tail ys)
